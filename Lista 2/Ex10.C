83f89db0e3c62554c0ac308220c2b0176b7842ca
Gabriel Ferreira Silva Matos <gferreiramatos12@gmail.com>
2026-09-20 16:05:51 -0300

    Create Ex10.C

---
+++ Lista 2/Ex10.C
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main(int argc, char *argv[]) {
+	int n;
+	printf("Entre com o valor de N");
+	scanf("%d", &n);
+	print("o numero %d, seu antecessor %d e seu sucessor %d",n, n-1, n+1);
+	return 0;
+	
+	
+	int a,b,c, maiorTemp, maior;
+	printf("insira tres valores para identificar o maior: ");
+	scanf("%d %d %d", &a,&b,&c);
+	
+	maiorTemp = ((a+b+abs(a-b))/2);
+	
+	maior = ((maiorTemp+c+abs(maiorTemp-c))/2);
+	
+	printf("o maior entre |%d|%d|%d|=%d", a,b,c, maiorTemp);
+	
+	
+	return 0;
+}
